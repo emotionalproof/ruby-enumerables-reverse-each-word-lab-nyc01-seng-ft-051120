524c2f53a07ef704458ae91d97ff2fdd3b147636
Chase <chaserabenn@gmail.com>
2020-05-07 15:18:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,6 +12,5 @@ def reverse_each_word(sentence)
 end
 
 def reverse_each_word(sentence)
-  reversed_sentence = sentence.split(" ").collect { |word| word.reverse}
-  reversed sentence.join(" ")
+  sentence.split(" ").collect { |word| word.reverse}.join(" ")
 end
