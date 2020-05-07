4f3b82b33285910e1a892534b1adfd306c5c2b19
Chase <chaserabenn@gmail.com>
2020-05-07 15:05:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   reversed_sentence = ""
 
   sentence.split(" ").each do |word|
-    reversed_word = ""
+    reversed_word = word.split("")
     word.each_char do |letter|
       reversed_word.unshift(letter)
     end
